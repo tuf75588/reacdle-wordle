3dbedc5810e4d46c13e4d1a01a06c48c9f567c11
Andrew Davis <atd285@gmail.com>
2022-02-19 21:39:58 -0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,23 @@
 set -e
 
 # build
-yarn run build
+npm run build
 
 # navigate into the build output directory
 cd dist
 
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
 git init
+git checkout -b main
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:tuf75588/reacdle-wordle.git main:gh-pages
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:tuf75588/reacdle-wordle.git gh-pages
 
 cd -
